bc74cc2704a6dd6f2722d52e28b655c4fce98b6d
Nimesha-Niranji <kanathegedaranimeshaniranji@gmail.com>
2023-11-14 23:35:38 +0530

    Complete the UI part of the product app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import React from 'react';
 import ProductData from './ProductData';
 
 function App() {
+
+  const currentHour = new Date().getHours() > 9 ? new Date().getHours() : '0' + new Date().getHours();
+  const currentMinite = new Date().getMinutes() > 9 ? new Date().getMinutes() : '0' + new Date().getMinutes();
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,29 +16,43 @@ function App() {
       </header>
 
       <div className='mainContainer'>
+
         <div className='productPreview'>
           <img src='https://imgur.com/iOeUBV7.png' alt='Product Preview'/>
-          <div className='featureData'>
-            <p>{`${new Date().getHours()}:${new Date().getMinutes()}`}</p>
+          
+          <div className='timeSection'>
+            <p>{`${currentHour}:${currentMinite}`}</p>
           </div>
+
+          <div className='heartBeatSection'>
+            <i className="fa-solid fa-heart-pulse"></i>
+            <p>78</p>
+          </div>
+
         </div>
+
         <div className='productData'>
           <h1 className='productTitle'>{ProductData.title}</h1>
           <p className='productDes'>{ProductData.description}</p>
           <h3 className='sectionHeading'>Select Color</h3>
+
           <div>
             <img className={['productImg', 'selectedProductImg'].join(' ')} src='https://imgur.com/PTgQlim.png' alt='Red Colored Watch'/>
             <img className='productImg' src='https://imgur.com/Mplj1YR.png' alt='Blue Colored Watch'/>
             <img className='productImg' src='https://imgur.com/xSIK4M8.png' alt='Purple Colored Watch'/>
             <img className='productImg' src='https://imgur.com/iOeUBV7.png' alt='Black Colored Watch'/>
           </div>
+
           <h3 className='sectionHeading'>Features</h3>
+
           <div>
             <button className={['featureItem','selectedFeatureItem'].join(' ')}>Time</button>
             <button className='featureItem'>Heart rate</button>
           </div>
+
           <button className='primaryBtn'>Buy Now</button>
         </div>
+
       </div>
 
     </div>
